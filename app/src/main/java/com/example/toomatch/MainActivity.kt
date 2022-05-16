71a450e2b521607ae2756cb52f878fdf1d0d6382
framarcol <mecamcam@hotmail.com>
2022-05-17 08:40:02 +0900

    Creating Main Menu

--- app/src/main/java/com/example/toomatch/MainActivity.kt
+++ app/src/main/java/com/example/toomatch/MainActivity.kt
@@ -3,41 +3,55 @@ package com.example.toomatch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.toomatch.ui.theme.ToomatchTheme
+import androidx.compose.material.Text as MyText
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ToomatchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContent(){
+            MyTexts()
         }
+
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyTexts(){
+    Column {
+        Title("TOOMATCH")
+        SimpleButton()
+        SimpleButton()
+        SimpleButton()
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ToomatchTheme {
-        Greeting("Android")
+fun SimpleButton() {
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        MyText(text = "Simple Button")
     }
+}
+
+
+@Composable
+fun Title(text: String){
+    MyText(text)
+}
+
+@Preview
+@Composable
+fun PreviewTexts(){
+    MyTexts()
 }
